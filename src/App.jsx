1d1993525ec959966fea8a31e0997f8dd2fa1803
=chundrubharat6699@gmail.com <BharatChundru1@gmail.com>
2024-10-23 14:23:27 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Navbar from './Component/Navbar'
+import Hero from './Component/Hero/Hero'
+import Edu from './Component/Edu/Edu'
+import Skills from './Component/Skills/Skills'
+import Contact from './Component/Contact/Contact'
+import Footer from './Component/Footer/Footer'
+import Experince from './Component/Experince/Experince'
+import Projects from './Component/Projects/Projects'
+import Business from './Component/Business/Business'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Edu/>
+      <Skills/>
+      <Experince/>
+      <Business/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+
+      
+      
+    </div>
+  )
+}
+
+export default App
