eaa623c160288c174bf41a7f3b7d47179e38feab
Bharat Chundru <67573513+bharat-0099@users.noreply.github.com>
2024-10-24 19:34:21 -0400

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Navbar from './Component/Navbar'
-import Hero from './Component/Hero/Hero'
-import Edu from './Component/Edu/Edu'
-import Skills from './Component/Skills/Skills'
-import Contact from './Component/Contact/Contact'
-import Footer from './Component/Footer/Footer'
-import Experince from './Component/Experince/Experince'
-import Projects from './Component/Projects/Projects'
-import Business from './Component/Business/Business'
-
+import Navbar from './Component/Navbar/Navbar';
+import Hero from './Component/Hero/Hero';
+import Edu from './Component/Edu/Edu';
+import Skills from './Component/Skills/Skills';
+import Contact from './Component/Contact/Contact';
+import Footer from './Component/Footer/Footer';
+import Experince from './Component/Experince/Experince';
+import Projects from './Component/Projects/Projects';
+import Business from './Component/Business/Business';
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      <Hero/>
-      <Edu/>
-      <Skills/>
-      <Experince/>
-      <Business/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
-
-      
-      
-    </div>
-  )
+    return (
+        <div>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Hero} />
+                <Route path="/edu" component={Edu} />
+                <Route path="/skills" component={Skills} />
+                <Route path="/experience" component={Experince} />
+                <Route path="/business" component={Business} />
+                <Route path="/projects" component={Projects} />
+                <Route path="/contact" component={Contact} />
+            </Switch>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
